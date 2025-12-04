df9adcb8f38163470641f7725d7c49a75cb03cda
Nimesh_Dhumal <nimesh.dhumal@nationalexpress.com>
2025-12-04 17:03:30 +0530

    MVC folder structure created and DB connection configuration setup.

---
+++ src/config/db.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB, process.env.USER, process.env.PASSWORD, {
+    host: process.env.HOST,
+    dialect: process.env.DIALECT
+});
+
+//Check the connection;;;
+async function connection() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully');
+        // await sequelize.sync();
+        // console.log('Table created successfully!');
+    } catch (error) {
+        console.log('Unable to connect to the database:', error);
+        // console.log('Table not created successfully');
+    }
+}
+
+connection();
+
+module.exports = { sequelize };
